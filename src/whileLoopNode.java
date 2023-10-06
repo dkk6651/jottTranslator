b36b6f6bc01b8b898677ae2736af2f0e55d5d3d9
is4761 <is4761@rit.edu>
2023-10-06 12:13:06 -0400

    implemented parse function for while loop

--- src/whileLoopNode.java
+++ src/whileLoopNode.java
@@ -1,4 +1,64 @@
+import java.util.ArrayList;
+
 public class whileLoopNode implements JottTree {
+
+    public String value;
+    public JottTree condition;
+    public JottTree bodyNode;
+
+    public whileLoopNode(Token token){
+        this.value = token.getToken();
+    }
+
+    public static JottTree parse(ArrayList<Token> tokens) throws Exception {
+        Token token = tokens.get(0);
+        whileLoopNode node = null;
+        // check for while
+        if(token.getToken().equals("while")){
+            tokens.remove(0);
+            node = new whileLoopNode(token);
+            Token next_token = tokens.get(0);
+            //check for [
+            if (!next_token.getToken().equals("[")) {
+                throw new Exception("Syntax Error\n Token " + next_token.getToken() + " cannot be parsed into a [\n "
+                        + next_token.getFilename() + ":" + next_token.getLineNum());
+            }
+            else{
+                tokens.remove(0);
+                node.condition = exprNode.parse(tokens);
+                //check for ]
+                next_token = tokens.get(0);
+                if (!next_token.getToken().equals("]")) {
+                    throw new Exception("Syntax Error\n Token " + next_token.getToken() + " cannot be parsed into a ]\n "
+                            + next_token.getFilename() + ":" + next_token.getLineNum());
+                }
+                else{
+                    tokens.remove(0);
+                    next_token = tokens.get(0);
+                    //check for {
+                    if (!next_token.getToken().equals("{")) {
+                        throw new Exception("Syntax Error\n Token " + next_token.getToken() + " cannot be parsed into a {\n "
+                                + next_token.getFilename() + ":" + next_token.getLineNum());
+                    }
+                    else{
+                        tokens.remove(0);
+                        node.bodyNode = bodyStmtNode.parse(tokens);
+                        // check for }
+                        next_token = tokens.remove(0);
+                        if (!next_token.getToken().equals("}")) {
+                            throw new Exception("Syntax Error\n Token " + next_token.getToken() + " cannot be parsed into a }\n "
+                                    + next_token.getFilename() + ":" + next_token.getLineNum());
+                        }
+                    }
+
+                }
+
+            }
+        }
+        return node;
+    }
+
+
     @Override
     public String convertToJott() {
         return null;
