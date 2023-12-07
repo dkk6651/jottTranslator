6600e441f1c00717aa03ff0a8e81f56c0cdbebc6
JD Rears <jar6256@g.rit.edu>
2023-12-07 11:34:15 -0500

    phase4

--- src/treeNode/funcDefParamsTNode.java
+++ src/treeNode/funcDefParamsTNode.java
@@ -8,34 +8,37 @@ import provided.TokenType;
 import java.util.ArrayList;
 
 /**
-Author: JD Rears jar6256
+ * Author: JD Rears jar6256
  */
 
 public class funcDefParamsTNode implements JottTree {
     JottTree id;
     JottTree type;
 
-    public funcDefParamsTNode(){}
-    public static JottTree parse(ArrayList<Token> tokens) throws Exception{
+    public funcDefParamsTNode() {
+    }
+
+    public static JottTree parse(ArrayList<Token> tokens) throws Exception {
         Token token = tokens.get(0);
         funcDefParamsTNode node = new funcDefParamsTNode();
-        if (token.getTokenType() == TokenType.R_BRACKET){
+        if (token.getTokenType() == TokenType.R_BRACKET) {
             return null;
-        }
-        else if(token.getTokenType() != TokenType.COMMA){
-            throw new Exception(String.format("Syntax Error\nParameters missing comma\n%s:%d", token.getFilename(), token.getLineNum()));
+        } else if (token.getTokenType() != TokenType.COMMA) {
+            throw new Exception(String.format("Syntax Error\nParameters missing comma\n%s:%d", token.getFilename(),
+                    token.getLineNum()));
         }
         tokens.remove(0);
         node.id = idNode.parse(tokens);
-        if(tokens.get(0).getTokenType() != TokenType.COLON){
-            throw new Exception(String.format("Syntax Error\nParameters missing colon\n%s:%d", token.getFilename(), token.getLineNum()));
+        if (tokens.get(0).getTokenType() != TokenType.COLON) {
+            throw new Exception(String.format("Syntax Error\nParameters missing colon\n%s:%d", token.getFilename(),
+                    token.getLineNum()));
         }
         tokens.remove(0);
         node.type = typeNode.parse(tokens);
 
         return node;
     }
-    
+
     @Override
     public String convertToJott() {
         return "," + this.id.convertToJott() + ":" + this.type.convertToJott();
@@ -43,7 +46,7 @@ public class funcDefParamsTNode implements JottTree {
 
     @Override
     public String convertToJava(String className) {
-        return null;
+        return "," + this.type.convertToJava(className) + this.id.convertToJava(className);
     }
 
     @Override
@@ -53,7 +56,7 @@ public class funcDefParamsTNode implements JottTree {
 
     @Override
     public String convertToPython() {
-        return null;
+        return "," + this.id.convertToPython();
     }
 
     @Override
